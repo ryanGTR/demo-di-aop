303b3422bfb1b5e8fbee4a8cfe13240bf820c9cc
Howard Wang <idontwannarock@gmail.com>
2022-11-20 22:51:31 +0800

    fix: account is locked logic

--- src/main/java/com/example/demodiaop/AuthenticationService.java
+++ src/main/java/com/example/demodiaop/AuthenticationService.java
@@ -21,15 +21,21 @@ public class AuthenticationService {
 
     public boolean isValid(String account, String password, String otp) {
         // check if account is locked
-        HttpRequest isLockedRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter/is-locked")).GET().build();
         HttpClient httpClient = HttpClient.newHttpClient();
+        boolean isLocked;
         try {
+            HttpRequest isLockedRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter/is-locked")).GET().build();
             HttpResponse<String> isLockedResponse = httpClient.send(isLockedRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
             if (isLockedResponse.statusCode() != 200) {
                 throw new AuthenticationException("check account is locked web api error, account: " + account);
+            } else {
+                isLocked = Boolean.parseBoolean(isLockedResponse.body());
             }
         } catch (IOException | InterruptedException e) {
             throw new AuthenticationException("check account is locked web api error, account: " + account, e);
+        }
+        if (isLocked) {
+            throw new AuthenticationException("account: " + account + " is locked");
         }
 
         // get password from database
@@ -68,9 +74,9 @@ public class AuthenticationService {
         }
 
         // get current otp
-        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/otp")).GET().build();
         String currentOtp;
         try {
+            HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/otp")).GET().build();
             HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
             if (response.statusCode() == 200) {
                 currentOtp = response.body();
@@ -84,8 +90,8 @@ public class AuthenticationService {
         // compare hashedPassword with passwordFromDb to validate
         if (hashedPassword.equals(passwordFromDb) && currentOtp.equals(otp)) {
             // reset fail counter when succeeded
-            HttpRequest resetFailCounterRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter/reset")).POST(HttpRequest.BodyPublishers.ofString(account, StandardCharsets.UTF_8)).build();
             try {
+                HttpRequest resetFailCounterRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter/reset")).POST(HttpRequest.BodyPublishers.ofString(account, StandardCharsets.UTF_8)).build();
                 HttpResponse<String> resetFailCounterResponse = httpClient.send(resetFailCounterRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
                 if (resetFailCounterResponse.statusCode() != 200) {
                     throw new AuthenticationException("reset fail counter web api error, account: " + account);
@@ -96,8 +102,8 @@ public class AuthenticationService {
             return true;
         } else {
             // increase fail counter when failed
-            HttpRequest addFailCounterRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter/add")).POST(HttpRequest.BodyPublishers.ofString(account, StandardCharsets.UTF_8)).build();
             try {
+                HttpRequest addFailCounterRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter/add")).POST(HttpRequest.BodyPublishers.ofString(account, StandardCharsets.UTF_8)).build();
                 HttpResponse<String> addFailCounterResponse = httpClient.send(addFailCounterRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
                 if (addFailCounterResponse.statusCode() != 200) {
                     throw new AuthenticationException("add fail counter web api error, account: " + account);
@@ -107,9 +113,9 @@ public class AuthenticationService {
             }
 
             // log fail count
-            HttpRequest getFailCountRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter")).GET().build();
             int failedCount;
             try {
+                HttpRequest getFailCountRequest = HttpRequest.newBuilder().uri(URI.create("https://example.com/fail-counter")).GET().build();
                 HttpResponse<String> getFailCountResponse = httpClient.send(getFailCountRequest, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
                 if (getFailCountResponse.statusCode() != 200) {
                     throw new AuthenticationException("get fail counter web api error, account: " + account);
@@ -123,10 +129,10 @@ public class AuthenticationService {
             logger.info(String.format("account: {} failed times: {}", account, failedCount));
 
             // notify account of authentication failure on Slack
-            Slack slack = Slack.getInstance();
-            MethodsClient methodClient = slack.methods("slackToken");
-            ChatPostMessageRequest messageRequest = ChatPostMessageRequest.builder().channel("#random").text("account: " + account + " failed to login.").build();
             try {
+                Slack slack = Slack.getInstance();
+                MethodsClient methodClient = slack.methods("slackToken");
+                ChatPostMessageRequest messageRequest = ChatPostMessageRequest.builder().channel("#random").text("account: " + account + " failed to login.").build();
                 methodClient.chatPostMessage(messageRequest);
             } catch (IOException | SlackApiException e) {
                 throw new AuthenticationException("Slack web api error, account: " + account, e);
